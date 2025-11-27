51d493b769153c401d9134c1c52ad0093c867392
AleksandraPrucnal <115213334+AleksandraPrucnal@users.noreply.github.com>
2025-11-27 21:28:31 +0100

    selenium

--- test_selenium_register.py
+++ test_selenium_register.py
@@ -14,13 +14,13 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-TEST_USERNAME = 'ola'
-TEST_EMAIL = 'ola@email.com'
-TEST_PASSWORD = 'ola123'
+USERNAME = 'ola'
+EMAIL = 'ola@email.com'
+PASSWORD = 'ola123'
 
-OTHER_TEST_USERNAME = 'ola2'
-OTHER_TEST_EMAIL = 'ola2@email.com'
-OTHER_TEST_PASSWORD = 'innehaslo'
+OTHER_USERNAME = 'ola2'
+OTHER_EMAIL = 'ola2@email.com'
+OTHER_PASSWORD = 'innehaslo'
 
 
 def _clean_database():
@@ -59,19 +59,19 @@ def driver():
 def test_register_success(driver, db_setup):
     driver.get("http://localhost:5000/auth/register")
 
-    driver.find_element(By.NAME, "username").send_keys(TEST_USERNAME)
-    driver.find_element(By.NAME, "email").send_keys(TEST_EMAIL)
-    driver.find_element(By.NAME, "password").send_keys(TEST_PASSWORD)
+    driver.find_element(By.NAME, "username").send_keys(USERNAME)
+    driver.find_element(By.NAME, "email").send_keys(EMAIL)
+    driver.find_element(By.NAME, "password").send_keys(PASSWORD)
 
     try:
         """szuka drugiego pola by_name 'password2',
         jesli takiego nie ma to by_css_selector czyli elementy zawierajace'password'
         drugie takie pole to powtorzenie hasla dlatego len>=2"""
-        driver.find_element(By.NAME, "password2").send_keys(TEST_PASSWORD)
+        driver.find_element(By.NAME, "password2").send_keys(PASSWORD)
     except:
         inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='password']")
         if len(inputs) >= 2:
-            inputs[1].send_keys(TEST_PASSWORD)
+            inputs[1].send_keys(PASSWORD)
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(1)
@@ -84,16 +84,16 @@ def test_register_success(driver, db_setup):
 def test_register_passwords_mismatch(driver, db_setup):
     driver.get("http://localhost:5000/auth/register")
 
-    driver.find_element(By.ID, "username").send_keys(TEST_USERNAME)
-    driver.find_element(By.ID, "email").send_keys(TEST_EMAIL)
-    driver.find_element(By.ID, "password").send_keys(TEST_PASSWORD)
+    driver.find_element(By.ID, "username").send_keys(USERNAME)
+    driver.find_element(By.ID, "email").send_keys(EMAIL)
+    driver.find_element(By.ID, "password").send_keys(PASSWORD)
 
     try:
-        driver.find_element(By.NAME, "check_password").send_keys(OTHER_TEST_PASSWORD)
+        driver.find_element(By.NAME, "check_password").send_keys(OTHER_PASSWORD)
     except:
         inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='password']")
         if len(inputs) >= 2:
-            inputs[1].send_keys(OTHER_TEST_PASSWORD)
+            inputs[1].send_keys(OTHER_PASSWORD)
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(1)
@@ -105,23 +105,23 @@ def test_register_passwords_mismatch(driver, db_setup):
 
 def test_register_username_exist(driver, db_setup):
     """Sprawdza, czy wykryje istniejacy username 'ola' i wyrzuci blad"""
-    existing_user = User(username=TEST_USERNAME, email=OTHER_TEST_EMAIL)
-    existing_user.set_password(TEST_PASSWORD)
+    existing_user = User(username=USERNAME, email=OTHER_EMAIL)
+    existing_user.set_password(PASSWORD)
     db.session.add(existing_user)
     db.session.commit()
 
     driver.get("http://localhost:5000/auth/register")
 
-    driver.find_element(By.NAME, "username").send_keys(TEST_USERNAME)
-    driver.find_element(By.NAME, "email").send_keys(TEST_EMAIL)
-    driver.find_element(By.NAME, "password").send_keys(OTHER_TEST_PASSWORD)
+    driver.find_element(By.NAME, "username").send_keys(USERNAME)
+    driver.find_element(By.NAME, "email").send_keys(EMAIL)
+    driver.find_element(By.NAME, "password").send_keys(OTHER_PASSWORD)
 
     try:
-        driver.find_element(By.NAME, "password2").send_keys(OTHER_TEST_PASSWORD)
+        driver.find_element(By.NAME, "password2").send_keys(OTHER_PASSWORD)
     except:
         inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='password']")
         if len(inputs) >= 2:
-            inputs[1].send_keys(OTHER_TEST_PASSWORD)
+            inputs[1].send_keys(OTHER_PASSWORD)
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(1)
@@ -133,23 +133,23 @@ def test_register_username_exist(driver, db_setup):
 
 def test_register_email_exist(driver, db_setup):
     """Sprawdza, czy wykryje istniejacy email i wyrzuci blad"""
-    existing_user = User(username=OTHER_TEST_USERNAME, email=TEST_EMAIL)
-    existing_user.set_password(TEST_PASSWORD)
+    existing_user = User(username=OTHER_USERNAME, email=EMAIL)
+    existing_user.set_password(PASSWORD)
     db.session.add(existing_user)
     db.session.commit()
 
     driver.get("http://localhost:5000/auth/register")
 
-    driver.find_element(By.NAME, "username").send_keys(TEST_USERNAME)
-    driver.find_element(By.NAME, "email").send_keys(TEST_EMAIL)
-    driver.find_element(By.NAME, "password").send_keys(OTHER_TEST_PASSWORD)
+    driver.find_element(By.NAME, "username").send_keys(USERNAME)
+    driver.find_element(By.NAME, "email").send_keys(EMAIL)
+    driver.find_element(By.NAME, "password").send_keys(OTHER_PASSWORD)
 
     try:
-        driver.find_element(By.NAME, "password2").send_keys(OTHER_TEST_PASSWORD)
+        driver.find_element(By.NAME, "password2").send_keys(OTHER_PASSWORD)
     except:
         inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='password']")
         if len(inputs) >= 2:
-            inputs[1].send_keys(OTHER_TEST_PASSWORD)
+            inputs[1].send_keys(OTHER_PASSWORD)
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(1)
@@ -163,16 +163,16 @@ def test_register_invalid_email_format(driver, db_setup):
     """sprawdza czy wyrzuci blad jesli podamy email w nieodpowiednim formacie"""
     driver.get("http://localhost:5000/auth/register")
 
-    driver.find_element(By.ID, "username").send_keys(TEST_USERNAME)
+    driver.find_element(By.ID, "username").send_keys(USERNAME)
     driver.find_element(By.ID, "email").send_keys("olaemail.com")
-    driver.find_element(By.ID, "password").send_keys(TEST_PASSWORD)
+    driver.find_element(By.ID, "password").send_keys(PASSWORD)
 
     try:
-        driver.find_element(By.NAME, "password2").send_keys(TEST_PASSWORD)
+        driver.find_element(By.NAME, "password2").send_keys(PASSWORD)
     except:
         inputs = driver.find_elements(By.CSS_SELECTOR, "input[type='password']")
         if len(inputs) >= 2:
-            inputs[1].send_keys(TEST_PASSWORD)
+            inputs[1].send_keys(PASSWORD)
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(1)
@@ -184,5 +184,4 @@ def test_register_invalid_email_format(driver, db_setup):
 
 
 if __name__ == "__main__":
-
     sys.exit(pytest.main(["-v", __file__]))
